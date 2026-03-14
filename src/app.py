6285b72759c8a7ec54bf6aacfa99c24c97e129c1
LilFreezyV <an.volodya.anufriev@yandex.ru>
2026-03-15 01:05:44 +0300

    time counter improved + som design

--- src/app.py
+++ src/app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, jsonify, request
-
-
 import os
 import time
 from pathlib import Path
@@ -28,7 +26,6 @@ mc = ModelController(temperature=1.3, max_completion_tokens=150)
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'POST':
-        # Получаем текст из формы
         user_text = request.form['user_text']
         start_time = time.time()
         answer = mc.generate(
@@ -36,42 +33,37 @@ def main():
             question=user_text,
         )
         end_time = time.time()
-        return render_template('base.html', answer=format_answer(answer), time=end_time - start_time)
-
+        delta = round(float(end_time - start_time), 1)
+        return render_template('base.html', answer=format_answer(answer), time=f'{str(delta)} сек.')
     return render_template('base.html', answer='1')
 
 
-# @app.route('/health')
-# def dummy():
-#     return render_template('healthcheck.html')
-
-
 # NOTE: тестовая функция для проверки работы модели
-@app.route('/test_model')
-def test_model():
-    context = """В Гороховой улице, в одном из больших домов, народонаселения которого стало бы на целый уездный город, лежал утром в постели, на своей квартире, Илья Ильич Обломов.
-
-Это был человек лет тридцати двух-трех от роду, среднего роста, приятной наружности, с темно-серыми глазами, но с отсутствием всякой определенной идеи, всякой сосредоточенности в чертах лица. Мысль гуляла вольной птицей по лицу, порхала в глазах, садилась на полуотворенные губы, пряталась в складках лба, потом совсем пропадала, и тогда во всем лице теплился ровный свет беспечности. С лица беспечность переходила в позы всего тела, даже в складки шлафрока. <...>
-
-И поверхностно наблюдательный, холодный человек, взглянув мимоходом на Обломова, сказал бы: «Добряк должен быть, простота!» Человек поглубже и посимпатичнее, долго вглядываясь в лицо его, отошел бы в приятном раздумье, с улыбкой."""
-
-    question = 'расскажи про Обломова'
-
-    start_time = time.time()
-
-    answer = mc.generate(
-        context=context,
-        question=question,
-    )
-
-    end_time = time.time()
-
-    return jsonify({
-        'question': question,
-        'context': context,
-        'answer': answer,
-        "time": end_time - start_time,
-    })
-
+# @app.route('/test_model')
+# def test_model():
+#     context = """В Гороховой улице, в одном из больших домов, народонаселения которого стало бы на целый уездный город, лежал утром в постели, на своей квартире, Илья Ильич Обломов.
+#
+# Это был человек лет тридцати двух-трех от роду, среднего роста, приятной наружности, с темно-серыми глазами, но с отсутствием всякой определенной идеи, всякой сосредоточенности в чертах лица. Мысль гуляла вольной птицей по лицу, порхала в глазах, садилась на полуотворенные губы, пряталась в складках лба, потом совсем пропадала, и тогда во всем лице теплился ровный свет беспечности. С лица беспечность переходила в позы всего тела, даже в складки шлафрока. <...>
+#
+# И поверхностно наблюдательный, холодный человек, взглянув мимоходом на Обломова, сказал бы: «Добряк должен быть, простота!» Человек поглубже и посимпатичнее, долго вглядываясь в лицо его, отошел бы в приятном раздумье, с улыбкой."""
+#
+#     question = 'расскажи про Обломова'
+#
+#     start_time = time.time()
+#
+#     answer = mc.generate(
+#         context=context,
+#         question=question,
+#     )
+#
+#     end_time = time.time()
+#
+#     return jsonify({
+#         'question': question,
+#         'context': context,
+#         'answer': answer,
+#         "time": end_time - start_time,
+#     })
+#
 
 app.run('0.0.0.0', port=5000, debug=True)
