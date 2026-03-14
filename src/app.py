085c1cab019ca000309513ca17f759d73300b41b
LilFreezyV <an.volodya.anufriev@yandex.ru>
2026-03-15 00:50:42 +0300

    added time counter

--- src/app.py
+++ src/app.py
@@ -1,21 +1,16 @@
 from flask import Flask, render_template, jsonify, request
 
+
 import os
 import time
 from pathlib import Path
 from model_controller import ModelController
-from scripts import format_answer
+from scripts import format_answer, get_context
 
 BASE_DIR = Path(__file__).resolve().parent
 
 TEMPLATES_DIR = BASE_DIR / 'templates'
 STATIC_DIR = BASE_DIR / 'static'
-
-context = """В Гороховой улице, в одном из больших домов, народонаселения которого стало бы на целый уездный город, лежал утром в постели, на своей квартире, Илья Ильич Обломов.
-
-Это был человек лет тридцати двух-трех от роду, среднего роста, приятной наружности, с темно-серыми глазами, но с отсутствием всякой определенной идеи, всякой сосредоточенности в чертах лица. Мысль гуляла вольной птицей по лицу, порхала в глазах, садилась на полуотворенные губы, пряталась в складках лба, потом совсем пропадала, и тогда во всем лице теплился ровный свет беспечности. С лица беспечность переходила в позы всего тела, даже в складки шлафрока. <...>
-
-И поверхностно наблюдательный, холодный человек, взглянув мимоходом на Обломова, сказал бы: «Добряк должен быть, простота!» Человек поглубже и посимпатичнее, долго вглядываясь в лицо его, отошел бы в приятном раздумье, с улыбкой."""
 
 app = Flask(
     'Book assistant',
@@ -35,13 +30,13 @@ def main():
     if request.method == 'POST':
         # Получаем текст из формы
         user_text = request.form['user_text']
+        start_time = time.time()
         answer = mc.generate(
-            context=context,
+            context=get_context(),
             question=user_text,
         )
-        print(user_text)
-        print(answer)
-        return render_template('base.html', answer=format_answer(answer))
+        end_time = time.time()
+        return render_template('base.html', answer=format_answer(answer), time=end_time - start_time)
 
     return render_template('base.html', answer='1')
 
