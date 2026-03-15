86d47e2b8a1dcb2a976dcf582250f7190bc7cf36
Kirill Churin <kirillchurin07@mail.ru>
2026-03-15 13:44:54 +0300

    Слегка изменена структура, добавлены шаблоны для будущих запросов и тп

--- src/app.py
+++ src/app.py
@@ -3,7 +3,11 @@ import os
 import time
 from pathlib import Path
 from model_controller import ModelController
+from pinecone_controller import PineconeController
 from scripts import format_answer, get_context
+from dotenv import load_dotenv
+
+
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -17,6 +21,7 @@ app = Flask(
 )
 
 mc = ModelController(temperature=1.3, max_completion_tokens=150)
+pc = PineconeController()
 
 
 @app.route('/')
@@ -24,57 +29,47 @@ def main():
     return render_template('main_page.html')
 
 
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    if request.method == 'POST':
-        user_text = request.form['user_text']
-        start_time = time.time()
-        answer = mc.generate(
-            context=get_context(),
-            question=user_text,
-        )
-        end_time = time.time()
-        delta = round(float(end_time - start_time), 1)
-        return render_template('search.html', answer=format_answer(answer), time=f'{str(delta)} сек.')
-    return render_template('search.html', answer='1')
+@app.route('/search', methods=['GET'])
+def search_show():
+    return render_template('search.html')
+    # if request.method == 'POST':
+    #     user_text = request.form['user_text']
+    #     start_time = time.time()
+    #     answer = mc.generate(
+    #         context=get_context(),
+    #         question=user_text,
+    #     )
+    #     end_time = time.time()
+    #     delta = round(float(end_time - start_time), 1)
+    #     return render_template('search.html', answer=format_answer(answer), time=f'{str(delta)} сек.')
+    # return render_template('search.html', answer='1')
 
+@app.route('/search', methods=['POST'])
+def search_post():
+    user_text = request.form['user_text']
+    result = pc.get_fragments('вопросик может не зайти)') # list[str] - найденные фрагменты
+    return render_template('search.html', answer=result)
 
 @app.route('/library')
 def show_lib():
     return render_template('library.html')
 
 
-@app.route('/question')
-def question():
+@app.route('/question', methods=['GET'])
+def question_show():
     return render_template('question.html')
 
+@app.route('/question', methods=['POST'])
+def question_post():
+    user_text = request.form['user_text']
+    start_time = time.time()
+    answer = mc.generate(
+        context=get_context(),
+        question=user_text,
+    )
+    end_time = time.time()
+    delta = round(float(end_time - start_time), 1)
+    return render_template('question.html', answer=format_answer(answer), time=f'{str(delta)} сек.')
 
-# NOTE: тестовая функция для проверки работы модели
-# @app.route('/test_model')
-# def test_model():
-#     context = """В Гороховой улице, в одном из больших домов, народонаселения которого стало бы на целый уездный город, лежал утром в постели, на своей квартире, Илья Ильич Обломов.
-#
-# Это был человек лет тридцати двух-трех от роду, среднего роста, приятной наружности, с темно-серыми глазами, но с отсутствием всякой определенной идеи, всякой сосредоточенности в чертах лица. Мысль гуляла вольной птицей по лицу, порхала в глазах, садилась на полуотворенные губы, пряталась в складках лба, потом совсем пропадала, и тогда во всем лице теплился ровный свет беспечности. С лица беспечность переходила в позы всего тела, даже в складки шлафрока. <...>
-#
-# И поверхностно наблюдательный, холодный человек, взглянув мимоходом на Обломова, сказал бы: «Добряк должен быть, простота!» Человек поглубже и посимпатичнее, долго вглядываясь в лицо его, отошел бы в приятном раздумье, с улыбкой."""
-#
-#     question = 'расскажи про Обломова'
-#
-#     start_time = time.time()
-#
-#     answer = mc.generate(
-#         context=context,
-#         question=question,
-#     )
-#
-#     end_time = time.time()
-#
-#     return jsonify({
-#         'question': question,
-#         'context': context,
-#         'answer': answer,
-#         "time": end_time - start_time,
-#     })
-#
 
 app.run('0.0.0.0', port=5000, debug=True)
---
+++ src/pinecone_controller.py
@@ -0,0 +1,15 @@
+
+
+class PineconeController:
+    def __init__(self):
+        pass
+    
+    # NOTE: пока заглушка
+    def get_fragments(self, question: str) -> list:
+        return [
+            "абоба1",
+            "абоба2",
+            "абоба3",
+            "абоба4",
+            "абоба5",
+        ]
