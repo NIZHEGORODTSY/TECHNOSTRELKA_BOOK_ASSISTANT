fbe60c9325b1f3c6e56e2620e288a7b2525855cf
LilFreezyV <an.volodya.anufriev@yandex.ru>
2026-03-15 22:27:55 +0300

    adding new book is ready

--- src/app.py
+++ src/app.py
@@ -121,7 +121,7 @@ def upload_file():
 
             file.save(file_path)
 
-            with open(file_path, 'r', encoding='utf-8') as f:
+            with open(file_path, 'r') as f:
                 data = f.read()
             
             pc.add_book(
